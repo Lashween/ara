6389cd2a95d5a624a6f82dd6f1e8f844950c00d9
Lashween <lashweenraj2806@gmail.com>
2021-11-23 19:10:49 +0800

    changed title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('iras app is running!');
+    expect(await page.getTitleText()).toEqual('ara app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/iras'),
+      dir: require('path').join(__dirname, './coverage/ara'),
       subdir: '.',
       reporters: [
         { type: 'html' },
